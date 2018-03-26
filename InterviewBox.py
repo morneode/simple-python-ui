308fdcaca0f01c67a465a0bcf92d91744bae0736
Morne Odendaal <morne@jemstep.com>
2018-03-26 15:08:07 +0200

    skeleton to be completed

--- InterviewBox.py
+++ InterviewBox.py
@@ -1,24 +1,38 @@
-from tkinter import *
+try:
+    import Tkinter as tk
+except:
+    import tkinter as tk
+
 import subprocess
 
 def show_entry_fields():
    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
    print
-   command = "./somebashfile.sh %s %s" % (e1.get(), e2.get())
+   name = e1.get()
+   surname = e2.get()
+#  branchName = createBranchName(name,surname)
+#  command = "./somebashfile.sh %s" % (branchname)
+   command = "./somebashfile.sh %s" % (name)
    subprocess.call(command,shell=True)
 
-master = Tk()
-master.wm_title("InterviewBox")
-Label(master, text="First Name").grid(row=0)
-Label(master, text="Last Name").grid(row=1)
+#  def createBranchName(name,surname):
+#    fix case of name (Name)
+#    fix case of surname (Surname)
+#    concat name and surname (NameSurname)
+#    return it
 
-e1 = Entry(master)
-e2 = Entry(master)
+master = tk()
+master.wm_title("InterviewBox")
+tk.Label(master, text="First Name").grid(row=0)
+tk.Label(master, text="Last Name").grid(row=1)
+
+e1 = tk.Entry(master)
+e2 = tk.Entry(master)
 
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
-Button(master, text='Exit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='Submit', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+tk.Button(master, text='Exit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
+tk.Button(master, text='Submit', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
 
-mainloop( )
+tk.mainloop( )
--- somebashfile.sh
+++ somebashfile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "running the bash script"
-echo "param 1(name): $1"
-echo "param 2(surname): $2"
+echo "param 1(NameSurname): $1"
